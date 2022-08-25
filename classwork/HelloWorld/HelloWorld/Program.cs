47537ce8f9eff01dfda512510325394d0fd4ed43
Claire Walker <claire.walker@my.tccd.edu>
2022-08-24 21:36:11 -0500

    Hello World and Section 1.

---
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// 
+// Claire Walker
+// ITSE 1430 2022 Fall
+// Lab 1
+//
+namespace HelloWorld
+{
+    /*
+     *  A multiline comment
+     */
+    internal class Program
+    {
+        // A comment
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
---
+++ classwork/Section1/Section1/Program.cs
@@ -0,0 +1,13 @@
+﻿int hours;
+
+Console.WriteLine("Hours: ");
+string value = Console.ReadLine();
+
+hours = Int32.Parse(value);
+
+Console.WriteLine("Pay rate: ");
+value = Console.ReadLine();
+
+double payRate = Double.Parse(value);
+
+Console.WriteLine("Your pay is " + (hours * payRate));
