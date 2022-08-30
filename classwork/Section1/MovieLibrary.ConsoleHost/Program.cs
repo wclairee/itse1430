785b5c06634344922fe30f8e6de8ec384b4f44cd
Claire Walker <claire.walker@my.tccd.edu>
2022-08-29 21:05:41 -0500

    Started movie library project Monday's class.

---
+++ classwork/Section1/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,14 @@
+﻿//Movie definition
+string title = "";
+string description = "";
+int runLength = 0; //in minutes
+int releaseYear = 1900;
+string rating = "";
+bool isClassic = false;
+
+Console.WriteLine("Enter a title: "); //to display
+Console.WriteLine("Enter an optional description: ");
+Console.WriteLine("Enter a run length (in minutes): ");
+Console.WriteLine("Enter the release year: ");
+Console.WriteLine("Entering MPAA rating: ");
+Console.WriteLine("Is this a classic? ");
--- classwork/Section1/Section1/Program.cs
+++ classwork/Section1/Section1/Program.cs
@@ -11,3 +11,24 @@ value = Console.ReadLine();
 double payRate = Double.Parse(value);
 
 Console.WriteLine("Your pay is " + (hours * payRate));
+
+int x;
+double distanceFromMoon = 0;
+//distanceFromMoon = 0;
+char letterGrade;
+bool isPassing;
+string name;
+
+string firstName = "Bob", lastName = "Smith";
+firstName = lastName = "Sam";
+//firstName = "Sam";
+//lastName = "Sam";
+
+// Block statement
+{
+    decimal payRate;
+
+}
+
+//distanceFromMoon = 10.5;
+isPassing = distanceFromMoon > 10;
