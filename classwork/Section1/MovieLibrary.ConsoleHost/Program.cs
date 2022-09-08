fa11ef57502ec6b6770d16ee1bc056291ae72815
Claire Walker <claire.walker@my.tccd.edu>
2022-09-07 21:27:21 -0500

    Wednesday Class-loops and operators

--- classwork/Section1/MovieLibrary.ConsoleHost/Program.cs
+++ classwork/Section1/MovieLibrary.ConsoleHost/Program.cs
@@ -8,44 +8,80 @@ bool isClassic = false;
 
 AddMovie();
 
-int ReadInt32 ( string message )
+bool ReadBoolean ( string message )
 {
     Console.Write(message);
 
-    string value = Console.ReadLine();
+    //Looking for Y/N
+    ConsoleKeyInfo key = Console.ReadKey();
+    if (key.Key == ConsoleKey.Y)
+        return true;
+    else if (key.Key == ConsoleKey.N)
+        return false;
 
-    //Inline variable declarations
-    //int result;
-    //if (Int32.TryParse(value, out result))
-    if (Int32.TryParse(value, out int result))
-        return result;
 
-    //TODO Loop
-    return -1;
+    //TODO: error
+    return false;
 }
 
-string ReadString ( string message )
+int ReadInt32 ( string message, int minimumValue, int maximumValue )
 {
     Console.Write(message);
 
-    string value = Console.ReadLine();
+    do
+    {
 
-    return value;
+   
+        string value = Console.ReadLine();
+
+        //Inline variable declarations
+        //int result;
+        //if (Int32.TryParse(value, out result))
+        if (Int32.TryParse(value, out int result))
+        {
+            if (result >= minimumValue && result <= maximumValue)
+            return result;
+        };
+
+        //if (false)
+            //break; //Exit loop
+            //continue; //Exit iteration
+
+        Console.WriteLine("Value must be between " + minimumValue + " and " + maximumValue);
+    } while (true);
+}
+
+string ReadString ( string message, bool required )
+{
+    //message = "Bob";
+    Console.Write(message);
+
+    while (true)
+    {
+        string value = Console.ReadLine();
+
+        //check if required
+        if (value != "" || !required)
+            return value;
+
+        //value is empty and required
+        Console.WriteLine("Value is required");
+    };
 }
 
 void AddMovie ()
 {
     //string title = "";
-    title = ReadString("Enter a title: ");
+    title = ReadString("Enter a title: ", true);
 
     //string description = "";
-    description = ReadString("Enter an optional description: ");
+    description = ReadString("Enter an optional description: ", false);
 
     //int runLength = 0; //in minutes
-    runLength = ReadInt32("Enter a run length (in minutes): ");
+    runLength = ReadInt32("Enter a run length (in minutes): ", 0, 300);
 
-    releaseYear = ReadInt32("Enter the release year: ");
-    rating = ReadString("Entering MPAA rating: ");
+    releaseYear = ReadInt32("Enter the release year: ", 1900, 2100);
+    rating = ReadString("Entering MPAA rating: ", true);
 
-    Console.WriteLine("Is this a classic? ");
+    isClassic = ReadBoolean("Is this a classic? ");
 }
