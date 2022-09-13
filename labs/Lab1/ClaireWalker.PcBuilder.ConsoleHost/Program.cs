392a04e03964deeaf37f7f0ff8abafcba2705f8b
Claire Walker <claire.walker@my.tccd.edu>
2022-09-12 19:23:53 -0500

    updated version. 9/12/2022

--- labs/Lab1/ClaireWalker.PcBuilder.ConsoleHost/Program.cs
+++ labs/Lab1/ClaireWalker.PcBuilder.ConsoleHost/Program.cs
@@ -1,10 +1,15 @@
 ﻿void DisplayMenu()
 {
+    //information displays when application starts
     Console.WriteLine("Claire Walker\n");
     Console.WriteLine("ITSE 1430\n");
+    Console.WriteLine("Fall 2022\n");
     //Console.WriteLine(); 
     //need to add current date
 
-
-
+    //menu displays for user 
+    //user input validation loop
+    //user input goes into switch statement
+    //if user selects quit, prompt confirm message (Y/N) case insensitive
+    //
 }
