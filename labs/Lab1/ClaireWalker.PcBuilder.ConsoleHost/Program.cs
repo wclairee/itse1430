927840a4c4a3f0ea2385da15490494bd4f79c51a
Claire Walker <claire.walker@my.tccd.edu>
2022-09-21 16:08:47 -0500

    Incomplete.

--- labs/Lab1/ClaireWalker.PcBuilder.ConsoleHost/Program.cs
+++ labs/Lab1/ClaireWalker.PcBuilder.ConsoleHost/Program.cs
@@ -1,26 +1,151 @@
-﻿DisplayInfo();
-DisplayMenu();
+﻿var done = false;
+decimal currentCartTotal = 0;
+var memoryChoice = 0;
+var processorChoice = 0;
 
+DisplayInfo();
 
+do
+{
+    var userResponse = DisplayMenu();
+    Console.WriteLine();
+} while (true);
+
+//Functions
 void DisplayInfo ()
 {
     //information displays when application starts
     Console.WriteLine("Claire Walker");
     Console.WriteLine("ITSE 1430");
     Console.WriteLine("Fall 2022");
-
+    Console.WriteLine();
 }
 
-char DisplayMenu ()
+string DisplayMenu ()
 {
-    Console.WriteLine("Press Q to quit.") //menu displays for user 
+    Console.WriteLine($"The current cart total is ${currentCartTotal}.");
+    Console.WriteLine("Press 1 to start a new order.");
+    Console.WriteLine("Press 2 to view your current order.");
+    Console.WriteLine("Press 3 to clear your current order.");
+    Console.WriteLine("Press 4 to modify your current order.");
+    Console.WriteLine("Press 5 to quit.");
+    do
+    {
+        string input = Console.ReadLine();
+        switch (input)
+        {
+            case ConsoleKey.N: return 'N';
+            case ConsoleKey.V: return 'V';
+            case ConsoleKey.C: return 'C';
+            case ConsoleKey.M: return 'M';
+            case ConsoleKey.Q: return 'Q';
+        };
+    } while (true);
+}
+
+void StartNewOrder ()
+{
+    decimal ryzen9 = 1410;
+    decimal ryzen7 = 1270;
+    decimal ryzen5 = 1200;
+    decimal intel9 = 1590;
+    decimal intel7 = 1400;
+    decimal intel5 = 1280;
+    currentCartTotal = 0;
+
+    Console.WriteLine();
+    Console.WriteLine("1.) AMD Ryzen 9 5900X\t$" + ryzen9);
+    Console.WriteLine("2.) AMD Ryzen 7 5700X\t$" + ryzen7);
+    Console.WriteLine("3.) AMD Ryzen 5 5600X\t$" + ryzen5);
+    Console.WriteLine("4.) Intel i9-12900K\t$" + intel9);
+    Console.WriteLine("5.) Intel i7-12700K\t$" + intel7);
+    Console.WriteLine("6.) Intel i5-12600K\t$" + intel5);
 
     do
     {
-       
-    //user input validation loop
-    //user input goes into switch statement
-    //if user selects quit, prompt confirm message (Y/N) case insensitive
-    //
+        processorChoice = ReadInt32("Which processor would you like to use? Select a number between 1-6", 1, 6);
+        if (processorChoice == 1)
+            currentCartTotal += ryzen9;
+        else if (processorChoice == 2)
+            currentCartTotal += ryzen7;
+        else if (processorChoice == 3)
+            currentCartTotal += ryzen5;
+        else if (processorChoice == 4)
+            currentCartTotal += intel9;
+        else if (processorChoice == 5)
+            currentCartTotal += intel7;
+        else if (processorChoice == 6)
+            currentCartTotal += intel5;
+            
     } while (true);
+}
+
+void CalculatePrice ()
+{
+
+}
+
+void ViewOrder ()
+{
+
+}
+
+void ModifyOrder()
+{ 
+
+}
+
+void ClearOrder()
+{
+
+}
+
+bool ReadBoolean ( string message )
+{
+    Console.Write(message);
+
+    do
+    {
+        ConsoleKeyInfo key = Console.ReadKey();
+        if (key.Key == ConsoleKey.Y)
+            return true;
+        else if (key.Key == ConsoleKey.N)
+            return false;
+    } while (true);
+}
+
+int ReadInt32 ( string message, int minimumValue, int maximumValue )
+{
+    Console.Write(message);
+
+    do
+    {
+
+        string value = Console.ReadLine();
+
+        if (Int32.TryParse(value, out var result))
+        {
+            if (result >= minimumValue && result <= maximumValue)
+                return result;
+        };
+
+        Console.WriteLine("Value must be between " + minimumValue + " and " + maximumValue);
+    } while (true);
+}
+
+string ReadString ( string message, bool required )
+{
+    Console.Write(message);
+
+    while (true)
+    {
+        string value = Console.ReadLine();
+
+        //check if required
+        if (value != "" || !required)
+            return value;
+
+        //value is empty and required
+        Console.WriteLine("Value is required");
+    };
 }
