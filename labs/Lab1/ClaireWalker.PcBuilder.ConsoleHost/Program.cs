c149465443335a9bb348ed3934cab12eb6695570
Claire Walker <claire.walker@my.tccd.edu>
2022-09-21 18:33:50 -0500

    Updated as of 6:30pm 9/21, incomplete

--- labs/Lab1/ClaireWalker.PcBuilder.ConsoleHost/Program.cs
+++ labs/Lab1/ClaireWalker.PcBuilder.ConsoleHost/Program.cs
@@ -1,14 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-var done = false;
-decimal currentCartTotal = 0;
+﻿decimal currentCartTotal = 0;
 var memoryChoice = 0;
 var processorChoice = 0;
+decimal ryzen9 = 1410;
+decimal ryzen7 = 1270;
+decimal ryzen5 = 1200;
+decimal intel9 = 1590;
+decimal intel7 = 1400;
+decimal intel5 = 1280;
+decimal memory8gb = 30;
+decimal memory16gb = 40;
+decimal memory32gb = 90;
+decimal memory64gb = 410;
+decimal memory128gb = 600;
 
 DisplayInfo();
 
 do
 {
+
     var userResponse = DisplayMenu();
     Console.WriteLine();
     if (userResponse == 1)
@@ -22,12 +31,11 @@ do
     else if (userResponse == 5)
     {
         bool answer = ReadBoolean("Are you sure you want to quit? Y/N");
-        if (answer == true)
-            break;
-        else if (answer == false)
-
-
+        if (answer != true)
+            continue;
+        break;
     }
+
 } while (true);
 
 //Functions
@@ -42,6 +50,7 @@ void DisplayInfo ()
 
 int DisplayMenu()
 {
+    Console.WriteLine();
     Console.WriteLine($"The current cart total is ${currentCartTotal}.");
     Console.WriteLine("Press 1 to start a new order.");
     Console.WriteLine("Press 2 to view your current order.");
@@ -58,12 +67,6 @@ int DisplayMenu()
 
 void StartNewOrder ()
 {
-    decimal ryzen9 = 1410;
-    decimal ryzen7 = 1270;
-    decimal ryzen5 = 1200;
-    decimal intel9 = 1590;
-    decimal intel7 = 1400;
-    decimal intel5 = 1280;
     currentCartTotal = 0;
 
     Console.WriteLine();
@@ -74,28 +77,53 @@ void StartNewOrder ()
     Console.WriteLine("5.) Intel i7-12700K\t$" + intel7);
     Console.WriteLine("6.) Intel i5-12600K\t$" + intel5);
 
-    do
-    {
-        processorChoice = ReadInt32("Which processor would you like to use? Select a number between 1-6", 1, 6);
-        if (processorChoice == 1)
-            currentCartTotal += ryzen9;
-        else if (processorChoice == 2)
-            currentCartTotal += ryzen7;
-        else if (processorChoice == 3)
-            currentCartTotal += ryzen5;
-        else if (processorChoice == 4)
-            currentCartTotal += intel9;
-        else if (processorChoice == 5)
-            currentCartTotal += intel7;
-        else if (processorChoice == 6)
-            currentCartTotal += intel5;
-            
-    } while (true);
+    processorChoice = ReadInt32("Which processor would you like to use? Select a number between 1-6", 1, 6);
+
+    Console.WriteLine();
+    Console.WriteLine("1.) 8 GB\t$" + memory8gb);
+    Console.WriteLine("2.) 16 GB\t$" + memory16gb);
+    Console.WriteLine("3.) 32 GB\t$" + memory32gb);
+    Console.WriteLine("4.) 64 GB\t$" + memory64gb);
+    Console.WriteLine("5.) 128 GB\t$" + memory128gb);
+
+    memoryChoice = ReadInt32("What memory storage would you like to have? Select a number between 1-5", 1, 5);
+
+    CalculatePrice(processorChoice, memoryChoice);
+
 }
 
-void CalculatePrice ()
+decimal CalculatePrice ( int processor, int memory )
 {
 
+    do
+    {
+        if (processor == 1)
+            currentCartTotal += ryzen9;
+        else if (processor == 2)
+            currentCartTotal += ryzen7;
+        else if (processor == 3)
+            currentCartTotal += ryzen5;
+        else if (processor == 4)
+            currentCartTotal += intel9;
+        else if (processor == 5)
+            currentCartTotal += intel7;
+        else if (processor == 6)
+            currentCartTotal += intel5;
+    } while (true);
+
+    do
+    {
+        if (memory == 1)
+            currentCartTotal += memory8gb;
+        else if (memory == 2)
+            currentCartTotal += memory16gb;
+        else if (memory == 3)
+            currentCartTotal = memory32gb;
+        else if (memory == 4)
+            currentCartTotal += memory64gb;
+        else if (memory == 5)
+            currentCartTotal += memory128gb;
+    } while (true);
 }
 
 void ViewOrder ()
