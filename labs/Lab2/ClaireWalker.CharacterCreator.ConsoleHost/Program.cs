6c109acaa10f6879708cc4797e533b9ae3bde920
Claire Walker <claire.walker@my.tccd.edu>
2022-09-26 17:24:31 -0500

    Story 1.

---
+++ labs/Lab2/ClaireWalker.CharacterCreator.ConsoleHost/Program.cs
@@ -0,0 +1,12 @@
+﻿//ITSE 1430 
+//Fall 2022
+//Claire Walker
+
+void DisplayInfo ()
+{
+    //information displays when application starts
+    Console.WriteLine("Claire Walker");
+    Console.WriteLine("ITSE 1430");
+    Console.WriteLine("Fall 2022");
+    Console.WriteLine();
+}
