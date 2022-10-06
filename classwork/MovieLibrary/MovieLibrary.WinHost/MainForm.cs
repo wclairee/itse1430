7648a24ed0d1ca29c4d42e14997cfb7f9fe4e23d
Claire Walker <claire.walker@my.tccd.edu>
2022-10-05 21:30:24 -0500

    Updated-Wed 10/5 winforms, namespaces, controls

---
+++ classwork/MovieLibrary/MovieLibrary.WinHost/MainForm.cs
@@ -0,0 +1,15 @@
+namespace MovieLibrary.WinHost
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMovieAdd ( object sender, EventArgs e )
+        {
+            MessageBox.Show("Add not implemented");
+        }
+    }
+}
