e37abbffe8cc31bbb20d8f5d8a24b34eb4b12295
Claire Walker <claire.walker@my.tccd.edu>
2022-12-05 21:21:44 -0600

    MVC, Controllers, Actions

---
+++ classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/MovieController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieLibrary.WebApp.Controllers
+{
+    //[Route("movies")]
+    public class MovieController : Controller
+    {
+        public MovieController ( IConfiguration configuration )
+        {
+            var connString = configuration.GetConnectionString("AppDatabase");
+
+            _database = new Sql.SqlMovieDatabase(connString);
+        }
+
+        //Action = public methods on controllers
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> Index ()
+        {
+            var movies = _database.GetAll();
+
+            //return Ok(movies);
+            return View(movies);
+        }
+
+        [HttpGet]
+        public ActionResult<Movie> Details ( int id )
+        {
+            var movie = _database.Get(id);
+            if (movie == null)
+                return NotFound();
+
+            return View(movie);
+        }
+
+        private readonly Sql.SqlMovieDatabase _database;
+    }
+}
